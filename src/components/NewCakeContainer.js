e385732572855a690a97225dd4cf478215bba6f5
jpcuentas123 <jpcuentas123@gmail.com>
2020-02-03 07:54:16 -0500

    Add Payload i Redux Action

--- src/components/NewCakeContainer.js
+++ src/components/NewCakeContainer.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { buyCake } from '../redux';
 
 
 export default function NewCakeContainer() {
+    const [number, setNumber] = useState(1)
     const numOfCakes = useSelector(state => state.cake.numOfCakes)
     const dispatch = useDispatch();
 
-    const buyCakeDispatch = () => {
-        if (numOfCakes > 0) { 
-            dispatch(buyCake())
+    const buyCakeDispatch = (number) => {
+        if (numOfCakes > 0 && numOfCakes>= number) {
+            dispatch(buyCake(number))
         }
     }
     return (
         <div>
             <h2>Num of Cakes - {numOfCakes}</h2>
-            <button onClick={() => buyCakeDispatch()}>Buy Cake</button>
+            <input type="number" value={number} onChange={e=>setNumber(e.target.value)}/>
+            <button onClick={() => buyCakeDispatch(number)}>Buy {number} Cake</button>
         </div>
     )
 }
